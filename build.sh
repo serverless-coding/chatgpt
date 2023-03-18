4daba6963aff8bc5fe95a4d8cf5c7f88b2ed5c9b
yiGmMk <marvelousme@163.com>
2023-03-19 01:05:46 +0800

    config

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 set -euxo pipefail
 
 mkdir -p dist
+copy ./public/index.html ./dist/index.html
 
 go build -o ./functions/chatgpt4 ./functions/main.go
 ls -al
@@ -9,3 +11,5 @@ GOBIN=$(pwd)/functions go install ./...
 ls -al ./functions/
 chmod +x "$(pwd)"/functions/*
 go env
+
+ls -al ./dist
